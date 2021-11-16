b97d07c891b67056f5603231f5b4b49d6851b633
Anastasiya Shadrina <anastasia.shadrina@jetbrains.com>
2021-11-17 05:14:30 +0700

    [FE, PSI2IR] Pass context receivers when calling typealias constructor

--- compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/codegen/IrBlackBoxCodegenTestGenerated.java
+++ compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/codegen/IrBlackBoxCodegenTestGenerated.java
@@ -15933,6 +15933,12 @@ public class IrBlackBoxCodegenTestGenerated extends AbstractIrBlackBoxCodegenTes
         }
 
         @Test
+        @TestMetadata("typealiasForContextualClass.kt")
+        public void testTypealiasForContextualClass() throws Exception {
+            runTest("compiler/testData/codegen/box/extensionClasses/typealiasForContextualClass.kt");
+        }
+
+        @Test
         @TestMetadata("useFromAnotherModule.kt")
         public void testUseFromAnotherModule() throws Exception {
             runTest("compiler/testData/codegen/box/extensionClasses/useFromAnotherModule.kt");
