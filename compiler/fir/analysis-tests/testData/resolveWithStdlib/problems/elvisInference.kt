a5e94344f079d58540a3a3421fdddbbcf2441e3d
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2021-12-10 17:46:12 +0300

    ++ simplification

--- compiler/fir/analysis-tests/testData/resolveWithStdlib/problems/elvisInference.kt
+++ compiler/fir/analysis-tests/testData/resolveWithStdlib/problems/elvisInference.kt
@@ -7,29 +7,11 @@ public class Generator {
     public <T extends Value> T createValue(@NotNull String content) {
         return (T) (new Value(content));
     }
-
-    @NotNull
-    public Obj createObject(@NotNull String content) {
-        return new Obj(content);
-    }
 }
 
 // FILE: test.kt
 open class Value(val s: String)
 
-class Obj(s: String) : Value(s)
-
 val generator = Generator()
 
-fun doIt(s: String?) =
-    (generator.<!NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>createValue<!>(s ?: "{}") as? Obj).also {
-        if (it == null) {
-            println()
-        }
-    } ?: generator.createObject("")
-
-val x by lazy {
-    generator.<!NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>createValue<!>("AlphaBeta") as Obj
-}
-
-val y = generator.<!NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>createValue<!>("Omega") as Obj
+val y = generator.<!NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>createValue<!>("Omega") as Value
