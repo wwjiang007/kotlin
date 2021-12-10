dafa22f8b81c693d0f609b241f7a2947c5747c57
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2021-12-10 17:37:14 +0300

    ++ Kotlin -> Java (!!!)

--- compiler/fir/analysis-tests/testData/resolveWithStdlib/problems/elvisInference.kt
+++ compiler/fir/analysis-tests/testData/resolveWithStdlib/problems/elvisInference.kt
@@ -1,22 +1,35 @@
+// FILE: Generator.java
+
+import org.jetbrains.annotations.NotNull;
+
+public class Generator {
+    @NotNull
+    public <T extends Value> T createValue(@NotNull String content) {
+        return (T) (new Value(content));
+    }
+
+    @NotNull
+    public Obj createObject(@NotNull String content) {
+        return new Obj(content);
+    }
+}
+
+// FILE: test.kt
 open class Value(val s: String)
 
 class Obj(s: String) : Value(s)
 
-object Generator {
-    fun <T : Value> createValue(s: String): T = (if (s.isEmpty()) Obj(s) else Value(s)) <!UNCHECKED_CAST!>as T<!>
-
-    fun createObject(s: String) = Obj(s)
-}
+val generator = Generator()
 
 fun doIt(s: String?) =
-    (Generator.createValue(s ?: "{}") as? Obj).also {
+    (generator.<!NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>createValue<!>(s ?: "{}") as? Obj).also {
         if (it == null) {
             println()
         }
-    } ?: Generator.createObject("")
+    } ?: generator.createObject("")
 
 val x by lazy {
-    Generator.createValue("AlphaBeta") as Obj
+    generator.<!NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>createValue<!>("AlphaBeta") as Obj
 }
 
-val y = Generator.createValue("Omega") as Obj
+val y = generator.<!NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>createValue<!>("Omega") as Obj
