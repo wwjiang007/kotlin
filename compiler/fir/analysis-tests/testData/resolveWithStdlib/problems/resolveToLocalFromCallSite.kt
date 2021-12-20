0e34a11e3f48b86ba2ab71b5bdf08ceb795d25f4
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2021-12-20 13:00:01 +0300

    ++ test enhancement

--- compiler/fir/analysis-tests/testData/resolveWithStdlib/problems/resolveToLocalFromCallSite.kt
+++ compiler/fir/analysis-tests/testData/resolveWithStdlib/problems/resolveToLocalFromCallSite.kt
@@ -1,9 +1,10 @@
 val x = object {
     fun foo(types: List<String>) {
+        val length = "123"
         types.mapIndexed { i, length -> Triple(i, length, length.getFilteredType()) }
     }
 
-    private fun String.getFilteredType() = bar(length)
+    private fun String.getFilteredType() = bar(<!ARGUMENT_TYPE_MISMATCH!>length<!>)
 }
 
 fun bar(x: Int) = x
