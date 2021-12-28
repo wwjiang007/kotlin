fb64ed275a06202a39f00dc9a274fecd1f4b7564
Denis.Zharkov <denis.zharkov@jetbrains.com>
2021-12-28 18:05:24 +0300

    FIR: Weaken some UPPER_BOUND_VIOLATED restrictions
    See test data at starProjectionInsteadOutCaptured.kt
    
    ^KT-49412 Fixed
    ^KT-50230 Relates

---
+++ compiler/testData/diagnostics/tests/typeParameters/starProjectionInsteadOutCaptured.kt
@@ -0,0 +1,13 @@
+// FIR_IDENTICAL
+// SKIP_TXT
+interface A
+interface B<X : A>
+interface C<E : A, F : B<E>>
+
+// FE1.0: OK
+// In FIR: Type argument is not within its bounds: ...
+fun foo1(c: C<out A, out B<*>>) {}
+
+fun foo2(c: C<*, B<*>>) {}
+fun <T : B<*>> foo3(c: C<*, T>) {}
+fun <T : B<*>> foo4(c: C<out A, T>) {}
